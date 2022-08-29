486ef03103bff13e7d1d041d3f5a581e988f49cc
Quwin <ethantran.et77@gmail.com>
2022-08-28 19:35:22 -0700

    Added Button, Css, JS

---
+++ script.js
@@ -0,0 +1,5 @@
+// On click for button
+function onClick() {
+    // Go to order link
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSd9UaFyvd60zfd81FSM7KoHhLrE5fInKHG-jGCRyHSRgeWmRw/viewform?usp=sf_link';
+}
